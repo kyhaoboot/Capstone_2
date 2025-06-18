09e84d6d0df981306c74064c73fc08d409d490cf
kyhaoboot <kyhaoboot@gmail.com>
2025-06-19 01:51:21 +0700

    Phần 2 Admin Pages, 2. chức năng thêm sản phẩm, 3. chức năng xóa sản phẩm

--- admin/controller/main.js
+++ admin/controller/main.js
@@ -48,11 +48,9 @@ const renderListProduct = (data) => {
               <td class="px-6 py-4">${product.desc}</td>
               <td class="px-6 py-4">${product.type}</td>
               <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >Edit</a
-                >
+               <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sửa</button>
+                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                onclick="onDeleteProduct(${product.id})">Xóa</button>
               </td>
             </tr>
     `;
@@ -61,3 +59,82 @@ const renderListProduct = (data) => {
 };
 
 getListProduct();
+
+// Xóa sản phẩm
+
+const onDeleteProduct = (id) => {
+  const promise = services.deleteProductApi(id);
+  promise
+    .then((result) => {
+      alert(`Delete Product ${result.data.name} success!`);
+      getListProduct();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// Khai báo hàm onDeleteProduct với đối tượng window
+window.onDeleteProduct = onDeleteProduct;
+
+// Open modal
+getEle("btnThemSP").onclick = function () {
+  // update title modal
+  document.getElementsByClassName("modal-title")[0].innerHTML = "Thêm sản phẩm";
+  // Create button add Product
+  const btnAdd = ` <button
+                 
+                  class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  onclick="onAddProduct()"
+                >
+                  Thêm sản phẩm mới
+                </button>`;
+  document.getElementsByClassName("modal-footer")[0].innerHTML = btnAdd;
+};
+
+// Lấy dữ liệu từ các thẻ input nhập vào
+const getValue = () => {
+  // Dom tới các thẻ input lấy value
+  const name = getEle("nameProduct").value;
+  const price = getEle("priceProduct").value;
+  const screen = getEle("screenProduct").value;
+  const backCamera = getEle("backProduct").value;
+  const frontCamera = getEle("frontProduct").value;
+  const img = getEle("imgProduct").value;
+  const type = getEle("typeProduct").value;
+  const desc = getEle("descProduct").value;
+  // khoi tạo đối tượng product từ lớp đối tượng product
+  const product = new Product(
+    "",
+    name,
+    price,
+    screen,
+    backCamera,
+    frontCamera,
+    img,
+    type,
+    desc
+  );
+  return product;
+};
+
+// hàm khởi tạo nút thệ sản phẩm mới
+const onAddProduct = () => {
+  console.log("123");
+  const product = getValue();
+  const promise = services.addProductApi(product);
+  promise
+    .then((result) => {
+      console.log(result);
+      // Thông báo thêm sản phẩm thành công
+      alert(`Add product ${result.data.name} success`);
+      // render lại list sản phẩm
+      getListProduct();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(product);
+};
+// khai báo hàm onAddProduct với đối tượng window
+window.onAddProduct = onAddProduct;
--- admin/services/product-services.js
+++ admin/services/product-services.js
@@ -6,6 +6,21 @@ class ProductServices {
     });
     return promise;
   }
+  deleteProductApi(id) {
+    const promise = axios({
+      url: `https://684931ef45f4c0f5ee7063db.mockapi.io/api/Product_CapStone_2/${id}`,
+      method: "DELETE",
+    });
+    return promise;
+  }
+  addProductApi(product) {
+    const promise = axios({
+      url: `https://684931ef45f4c0f5ee7063db.mockapi.io/api/Product_CapStone_2`,
+      method: "POST",
+      data: product,
+    });
+    return promise;
+  }
 }
 
 export default ProductServices;
